1c272d48e18d8b41ecaff0f670957727da0a2679
SELÇUK ILDEM <ildemselcuk@gmail.com>
2024-02-12 03:57:04 +0300

    fixed.

--- GUI/driver/views.py
+++ GUI/driver/views.py
@@ -2,25 +2,34 @@ import requests
 from django.shortcuts import render
 from .forms import DriverForm
 
+# Base URL for the FastAPI endpoint
 BASE_API_URL = 'http://api.example.com:5000'
 
+
 def _extend_api(path):
+    """
+    Helper function to extend the base API URL with a given path.
+    """
     return BASE_API_URL + path
 
+
 def get_driver_data(request):
+    """
+    View function to handle GET requests for driver data.
+    """
     if request.method == 'GET':
-        # Formu oluştur
+        # Create the form
         form = DriverForm(request.GET)
         if form.is_valid():
-            # Formdan gelen verileri al
+            # Get data from the form
             min_date = form.cleaned_data.get('startDate')
             max_date = form.cleaned_data.get('endDate')
             min_score = form.cleaned_data.get('minScore')
             max_score = form.cleaned_data.get('maxScore')
             limit = form.cleaned_data.get('limit')
             offset = form.cleaned_data.get('offset')
-            
-            # API isteği gönder
+
+            # Send API request
             r = requests.get(
                 _extend_api('/drivers'),
                 params={
@@ -32,16 +41,16 @@ def get_driver_data(request):
                     'offset': offset
                 }
             )
-            # Yanıtı işle
+            # Process the response
             if r.status_code == 200:
                 data = r.json().get('records', [])
             else:
                 data = []
-            # Template'e veriyi gönder
+            # Send the data to the template
             context = {'drivers': data, 'form': form}
             return render(request, "driver_list.html", context)
     else:
-        # Hatalı formu işle
+        # Handle invalid form
         form = DriverForm()
-    # Eğer GET isteği gönderilmediyse veya form geçerli değilse, boş bir form ile sayfayı yeniden yükle
+    # If no GET request was made or the form is invalid, reload the page with an empty form
     return render(request, "driver_list.html", {'form': form})
