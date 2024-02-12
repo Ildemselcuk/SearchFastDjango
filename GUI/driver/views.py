73b51dfd829c0d8ae7c25a00430684922b30fa82
SELÇUK ILDEM <ildemselcuk@gmail.com>
2024-02-12 03:59:36 +0300

    unnecessary code removed.

--- GUI/driver/views.py
+++ GUI/driver/views.py
@@ -17,40 +17,37 @@ def get_driver_data(request):
     """
     View function to handle GET requests for driver data.
     """
-    if request.method == 'GET':
-        # Create the form
-        form = DriverForm(request.GET)
-        if form.is_valid():
-            # Get data from the form
-            min_date = form.cleaned_data.get('startDate')
-            max_date = form.cleaned_data.get('endDate')
-            min_score = form.cleaned_data.get('minScore')
-            max_score = form.cleaned_data.get('maxScore')
-            limit = form.cleaned_data.get('limit')
-            offset = form.cleaned_data.get('offset')
 
-            # Send API request
-            r = requests.get(
-                _extend_api('/drivers'),
-                params={
-                    'startDate': min_date,
-                    'endDate': max_date,
-                    'minScore': min_score,
-                    'maxScore': max_score,
-                    'limit': limit,
-                    'offset': offset
-                }
-            )
-            # Process the response
-            if r.status_code == 200:
-                data = r.json().get('records', [])
-            else:
-                data = []
-            # Send the data to the template
-            context = {'drivers': data, 'form': form}
-            return render(request, "driver_list.html", context)
-    else:
-        # Handle invalid form
-        form = DriverForm()
+    # Create the form
+    form = DriverForm(request.GET)
+    if form.is_valid():
+        # Get data from the form
+        min_date = form.cleaned_data.get('startDate')
+        max_date = form.cleaned_data.get('endDate')
+        min_score = form.cleaned_data.get('minScore')
+        max_score = form.cleaned_data.get('maxScore')
+        limit = form.cleaned_data.get('limit')
+        offset = form.cleaned_data.get('offset')
+        # Send API request
+        r = requests.get(
+            _extend_api('/drivers'),
+            params={
+                'startDate': min_date,
+                'endDate': max_date,
+                'minScore': min_score,
+                'maxScore': max_score,
+                'limit': limit,
+                'offset': offset
+            }
+        )
+        # Process the response
+        if r.status_code == 200:
+            data = r.json().get('records', [])
+        else:
+            data = []
+        # Send the data to the template
+        context = {'drivers': data, 'form': form}
+        return render(request, "driver_list.html", context)
+
     # If no GET request was made or the form is invalid, reload the page with an empty form
     return render(request, "driver_list.html", {'form': form})
